0160a39c07e81dede557b620e17b267b1651a9e9
Fungi <2402020+FungiOfDeath@users.noreply.github.com>
2024-04-30 21:18:16 -0500

    incomplete work

--- src/compiler-passes/600-contification.js
+++ src/compiler-passes/600-contification.js
@@ -1,54 +1,109 @@
 import { map_subforms } from '../utils/visitors.js';
 
-export function contify(exp) {
-  const calls = new Map();
-  const usages = new Map();
-  const bindings = new Map();
-  const defs = new Map();
-  const sets = new Map();
-  const dag = gather_data(exp, undefined, calls, usages, bindings, defs, sets);
+export function contify() {
+  // collect stuffies
+  // find all calls
+  // find all dead functions
+  // remove tail calls from calls
+  // for each function, check that all of its calls are equal
+  // if true: contify
+  // repeat
 
-  const vertices = [];
-  const edges = [];
+  const functions = new Set();  // function names
+  const references = new Map(); // function name -> parent node of var
+  const funargs = new Map();    // function name -> nodes containing the name as an argument
+  const calls = new Map();      // function name -> call { node, parent_function }s
 
-  for (const [_, bind] of bindings) {
-    if (bind.value.$ !== 'lambda') continue;
-    vertices.push(bind);
+  // due to letrec* compilation, the sccs are stored in labels nodes
+  const sccs = new Map(); // function name -> (scc:) List[function name]
+
+  const alive = function_name => references.has(function_name);
+
+  for (const scc of sccs) {
+    const F = new Set();
+    for (const f of scc) {
+      for (const g of scc) {
+        // were all calls to F either a tail call or have the same continuations
+        let in_F = true;
+        let firstk, firsth;
+        // note the reversed order because we're looking for calls to f
+        for (const call of calls2(g, f)) {
+          if (!call.tail_call) {
+            if (!firstk) {
+              // first call, always passes
+              firstk = call.arg_k;
+              firsth = call.arg_j;
+            } else if (call.arg_k !== firstk || call.arg_h !== firsth) {
+              // failed to equal
+              in_F = false;
+              break;
+            }
+          }
+        }
+        if (in_F) {
+          F.add(f);
+        }
+      }
+    }
+    for (const f of F) {
+      for (const { node, parent_function } of calls.get(f) ?? []) {
+        if 
+      }
+    }
   }
-
-  // const funargs = new Set();
-  // const always_same_k = new Map();
-  // for (const [name, call_exps] of calls.entries()) {
-  //   for (const use of usages.get(name) ?? []) {
-  //     if (
-  //       use.parent.$ !== 'call' ||
-  //       use.args.some(arg => arg.$ === 'var' && arg.name === name)
-  //     ) {
-  //       funargs.add(name);
-  //     }
-  //   }
-  //   const k0 = call_exps[0].arg_k.name;
-  //   const has_always_same_k = call_exps
-  //     .slice(1)
-  //     .every(call => call.arg_k.name === k0);
-  //   if (has_always_same_k) {
-  //     always_same_k.set(name, k0);
-  //   }
-  // }
-
-  // console.dir(
-  //   {
-  //     calls: [...calls.entries()].map(([name, calls], idx) => ({
-  //       idx,
-  //       name,
-  //       calls: calls.length,
-  //     })),
-  //   },
-  //   { depth: 10 },
-  // );
-  // console.dir([...calls.entries()][27], { depth: 4 });
-  // console.log('Contifying', always_same_k);
+  function calls2(f, g) {} // returns the set of calls from f -> g
+  function tcssc(scc) {}
 }
+
+// export function contify(exp) {
+//   const calls = new Map();
+//   const usages = new Map();
+//   const bindings = new Map();
+//   const defs = new Map();
+//   const sets = new Map();
+//   const dag = gather_data(exp, undefined, calls, usages, bindings, defs, sets);
+
+//   const vertices = [];
+//   const edges = [];
+
+//   for (const [_, bind] of bindings) {
+//     if (bind.value.$ !== 'lambda') continue;
+//     vertices.push(bind);
+//   }
+
+//   // const funargs = new Set();
+//   // const always_same_k = new Map();
+//   // for (const [name, call_exps] of calls.entries()) {
+//   //   for (const use of usages.get(name) ?? []) {
+//   //     if (
+//   //       use.parent.$ !== 'call' ||
+//   //       use.args.some(arg => arg.$ === 'var' && arg.name === name)
+//   //     ) {
+//   //       funargs.add(name);
+//   //     }
+//   //   }
+//   //   const k0 = call_exps[0].arg_k.name;
+//   //   const has_always_same_k = call_exps
+//   //     .slice(1)
+//   //     .every(call => call.arg_k.name === k0);
+//   //   if (has_always_same_k) {
+//   //     always_same_k.set(name, k0);
+//   //   }
+//   // }
+
+//   // console.dir(
+//   //   {
+//   //     calls: [...calls.entries()].map(([name, calls], idx) => ({
+//   //       idx,
+//   //       name,
+//   //       calls: calls.length,
+//   //     })),
+//   //   },
+//   //   { depth: 10 },
+//   // );
+//   // console.dir([...calls.entries()][27], { depth: 4 });
+//   // console.log('Contifying', always_same_k);
+// }
 
 function add_to(map, name, value) {
   let found = map.get(name);
