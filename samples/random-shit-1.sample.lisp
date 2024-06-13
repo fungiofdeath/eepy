2203f824c8fd477999e96644d42984fa1ebccc72
Fungi <functionalfungi+git@posteo.com>
2024-06-13 03:09:27 -0500

    samples: update sample to not have name errors

--- samples/random-shit-1.sample.lisp
+++ samples/random-shit-1.sample.lisp
@@ -1,62 +1,65 @@
 
-(labels ((factorial (n) (factorial-iter n 1))
-         (factorial-iter (n accum)
-           (if (> n 0)
-               (factorial-iter (- n 1) (* n accum))
-               accum))
-         (fibonacci (n)
-           (labels ((iter (k a b)
-                     (if (= k n) a
-                         (iter (+ k 1) b (+ a b)))))
-             (iter 0 0 1)))
-         (make-counter (state)
+(labels ((factorial (n) (factorial/iter n 1))
+          (factorial/iter (n accum)
+            (if (= n 0)
+                accum
+              (factorial/iter (- n 1) (* n accum))))
+          (fibonacci (k) (fibonacci/iter k 0 1))
+          (fibonacci/iter (k a b)
+            (if (= k 0) a
+              (if (= k 1) b
+                (factorial/iter (- k 1) b (+ a b))))))
+  (factorial (fibonacci 10)))
+
+;; shadowed special forms example
+(let ((x (lambda () 1))
+      ;; we shadow lambda here:
+      (lambda (lambda (x body) (+ x body))))
+  ;; this should be a function call, not a lambda
+  (lambda (x) 2)
+  ;; this should refer to a variable named lambda
+  (set! lambda 3))
+
+(labels ((make-counter (state)
            (lambda ()
-             (let* ((saved state))
+             (let ((saved state))
                (set! state (+ 1 saved))
                saved))))
-  (let* ((k 1))
-    (let* ((k (+ k 1))
-           (k (+ k 2)))
-      (+ k 3))
-    k)
-  (letrec* ((k 1))
-    (letrec* ((a (compose b factorial))
-              (b (compose a fibonacci)))
-      a b)
-    k)
-  (letrec* ((a 1)
-            (b (letrec* ((c a))
-                 (letrec* ((d (b c)))
-                   (letrec* ()
-                     (block
-                       (letrec* ((k a))
-                         (lambda (b) k b))))))))
-    (+ a 2))
-  (let* ((a 1)
-         (b (let* ((c a))
-              (let* ((d (a c)))
-                (let* ()
-                  (block
-                    (let* ((k a))
-                      (lambda (b) k b))))))))
-    (+ a 2))
-  (letrec* ((a (compose b +))
-            (b (lambda (n)
-                 (if (> n 100)
-                     n
-                     (a n 10)))))
-    (a 10 12))
-  (letrec* ((a fun)
-            (b (compose a +))
-            (fun (lambda (n)
-                   (if (> n 100)
-                       n
-                       (b n 10)))))
-    (a 10 12))
-  (letrec* ((f (lambda (x) (set! f x) x)))
-    (f +)
-    (f 1 2))
-  (if (= 1 1) 1 2)
-  (print (factorial 10))
-  (print (fibonacci 10))
-  ())
+  (let ((count-1 (make-counter 0))
+        (count-2 (make-counter 1)))
+  (count-1)
+  (count-1)
+  (count-2)
+  (count-2)))
+
+(letrec* ((even (lambda (n)
+                  (if (= n 0) t
+                    (if (< n 0) nil
+                      (odd (- n 1))))))
+          (odd (lambda (n)
+                 (if (= n 1) t
+                   (if (< n 1) nil
+                     (even (- n 1)))))))
+  (even 20))
+
+(let* ((a 1)
+       (b 2)
+       (c 3)
+       (d 4))
+  (+ a b c d))
+
+(let* ((a 1)
+       (b (let* ((c a))
+            (let* ((d (a c)))
+              (let* ()
+                (block
+                  (let* ((k a))
+                    (lambda (b) k b))))))))
+  (= a b))
+
+(letrec* ((compose (lambda (f g) (lambda (x) (f (g x)))))
+          (a (compose a b))
+          (b (compose b a)))
+  (a))
+
+
